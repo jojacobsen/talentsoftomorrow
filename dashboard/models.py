9d8b8e14e0a319a000684be1eb3d7b6451c6fbd3
runjojo <jojo89>
2016-03-18 17:59:20 +0100

    Add Performance and Measurement model. Create Performance view and Serializers.

--- dashboard/models.py
+++ dashboard/models.py
@@ -7,10 +7,16 @@ from django.contrib.auth.models import User
 class Club(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
 
+    def __str__(self):
+        return self.user.username
+
 
 class Coach(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     club = models.ForeignKey(Club, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.user.username
 
 
 class Player(models.Model):
@@ -23,3 +29,24 @@ class Player(models.Model):
         ('F', 'Female'),
     )
     gender = models.CharField(max_length=10, choices=GENDER_CHOICES)
+
+    def __str__(self):
+        return self.user.username
+
+
+class Measurement(models.Model):
+    name = models.CharField(max_length=300)
+    unit = models.CharField(max_length=100)
+    description = models.CharField(max_length=2000)
+    upper_limit = models.DecimalField(max_digits=16, decimal_places=10)
+    lower_limit = models.DecimalField(max_digits=16, decimal_places=10)
+
+    def __str__(self):
+        return self.name
+
+
+class Performance(models.Model):
+    player = models.ForeignKey(Player, on_delete=models.CASCADE)
+    measurement = models.ForeignKey(Measurement, on_delete=models.CASCADE)
+    value = models.DecimalField(max_digits=16, decimal_places=10)
+    date = models.DateField(auto_now_add=True)
--- dashboard/urls.py
+++ dashboard/urls.py
@@ -10,4 +10,5 @@ urlpatterns = [
     url(r'^$', views.IndexView.as_view(), name='index'),
     url(r'^api-token-auth/', obtain_jwt_token),
     url(r'^api-token-refresh/', 'rest_framework_jwt.views.refresh_jwt_token'),
+    url(r'^performance/', views.PerformanceList.as_view(), name='performance'),
 ]
