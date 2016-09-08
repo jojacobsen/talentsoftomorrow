6976ea303fed686fc2b59ff2fd54cd815798d53f
runjojo <jojo89>
2016-09-08 11:40:09 +0200

    Exclude Media to sync.

--- deploy_scripts/afterInstall.bash
+++ deploy_scripts/afterInstall.bash
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
 
-rsync --delete-before --verbose --archive /home/django/envs/dashboard/release/ /home/django/envs/dashboard/talentstomorrow/ > /var/log/deploy.log
+rsync --delete-before --verbose --archive --exclude=dashboard/media /home/django/envs/dashboard/release/ /home/django/envs/dashboard/talentstomorrow/ > /var/log/deploy.log
+chown -R django:django /home/django/envs/dashbd/talentstomorrow
