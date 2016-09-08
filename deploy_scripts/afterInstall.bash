eb1a28388be973099b0a7e2631129002ff1e07f3
runjojo <jojo89>
2016-09-08 11:41:21 +0200

    fix path issue in deployment script.

--- deploy_scripts/afterInstall.bash
+++ deploy_scripts/afterInstall.bash
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 rsync --delete-before --verbose --archive --exclude=dashboard/media /home/django/envs/dashboard/release/ /home/django/envs/dashboard/talentstomorrow/ > /var/log/deploy.log
-chown -R django:django /home/django/envs/dashbd/talentstomorrow
+chown -R django:django /home/django/envs/dashboard/talentstomorrow
