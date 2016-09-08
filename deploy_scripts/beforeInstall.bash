6788a64341b14f02af8f9f0cfc923427492a73c7
runjojo <jojo89>
2016-09-08 11:22:50 +0200

    test djanog ownership of folder.

--- deploy_scripts/beforeInstall.bash
+++ deploy_scripts/beforeInstall.bash
@@ -4,3 +4,4 @@ if [ -d /home/django/envs/dashboard/ ]; then
     rm -rf /home/django/envs/dashboard/release
 fi
 mkdir -vp /home/django/envs/dashboard/release
+chown -R django:django /home/django/envs/dashboard/release
