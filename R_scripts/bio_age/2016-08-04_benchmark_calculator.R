00b45831ca80e336093d2e04d53ccc1adf593053
Lasse Folkersen <FOLK@lundbeck.com>
2016-10-01 21:16:52 +0200

    Adding direction

--- R_scripts/bio_age/2016-08-04_benchmark_calculator.R
+++ R_scripts/bio_age/2016-08-04_benchmark_calculator.R
@@ -2,9 +2,10 @@
 args<-commandArgs()
 
 #Args for testing stuff (not run)
-# value <- 40
-# population_mean <- 50
-# population_sd <- 5
+value <- 50.1
+population_mean <- 50
+population_sd <- 5
+direction <- "downBetter"
 
 
 
@@ -12,15 +13,24 @@ args<-commandArgs()
 value<-as.numeric(args[6])
 population_mean<-as.numeric(args[7])
 population_sd<-as.numeric(args[8])
+direction<-as.character(args[9])
 
 
 #Cheking input argument count
-if(length(args)!=8)stop("Must give 3 arguments exactly (value, population_mean, and population_sd)")
+if(length(args)!=9)stop("Must give 4 arguments exactly (value, population_mean, population_sd, direction)")
 
+if(any(is.na(value,population_mean,population_sd)))stop("value, population_mean, and population_sd must be given as numbers")
+
+if(!direction %in% c("upBetter","downBetter")){
+  stop("direction argument must be either upBetter or downBetter")
+}
 
 #Getting and cheking genetic_height_estimate argument
-percentage<-round(pnorm(value,mean=population_mean,sd=population_sd)*100,1)
-
+if(direction=="upBetter"){
+  percentage<-round(pnorm(value,mean=population_mean,sd=population_sd)*100,1)
+}else{
+  percentage<-100-round(pnorm(value,mean=population_mean,sd=population_sd)*100,1)
+}
 
 #output
 cat("benchmark_percent\n")
