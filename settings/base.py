c4de9f96157043b46dd4d196d0bdf270eb947f04
Johannes Jacob <johannespjacob@gmail.com>
2017-02-02 00:02:52 +0100

    increase jwt refresh delta.

--- settings/base.py
+++ settings/base.py
@@ -136,7 +136,7 @@ JWT_AUTH = {
     'JWT_ISSUER': None,
 
     'JWT_ALLOW_REFRESH': True,
-    'JWT_REFRESH_EXPIRATION_DELTA': datetime.timedelta(days=7),
+    'JWT_REFRESH_EXPIRATION_DELTA': datetime.timedelta(days=30),
 
     'JWT_AUTH_HEADER_PREFIX': 'JWT',
 }
