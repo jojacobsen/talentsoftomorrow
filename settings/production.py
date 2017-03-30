419650b70232c356b72d4d7aa243de409e786011
Johannes Jacob <johannespjacob@gmail.com>
2017-03-30 09:57:30 +0200

    Change Opbeat settings.

--- settings/production.py
+++ settings/production.py
@@ -3,3 +3,10 @@ from .base import *  # noqa
 
 ALLOWED_HOSTS = ['*.talentstomorrow.com']
 JWT_AUTH['JWT_EXPIRATION_DELTA'] = datetime.timedelta(days=7)
+
+OPBEAT = {
+    'ORGANIZATION_ID': 'fab3fd138af14d0e8f2b4ad7efcd22ce',
+    'APP_ID': '18d9e6610c',
+    'SECRET_TOKEN': '6799043939c43b57c3721338afd5b808fb88ec0a',
+}
+
