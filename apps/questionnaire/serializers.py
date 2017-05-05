a0a41f4d1240564f2a8748304f02e27554d33a94
Johannes Jacob <johannespjacob@gmail.com>
2017-05-05 16:41:16 +0200

    Improve performance.

--- apps/questionnaire/serializers.py
+++ apps/questionnaire/serializers.py
@@ -6,15 +6,15 @@ from questionnaire.models import Questionnaire
 class SubmissionSerializer(serializers.BaseSerializer):
     def to_representation(self, instance):
         answers = list()
-        for a in instance.answer_set.filter():
-            question = a.question
+        for a in instance.answer_set.values_list('answer', 'question__text', 'question__question_type',
+                                                 'question__id', 'question__slug', 'question__sort').filter():
             answers.append({
-                'question': question.text,
-                'question_type': question.question_type,
-                'question_id': question.id,
-                'question_slug': question.slug,
-                'question_sort': question.sort,
-                'answer': a.answer,
+                'question': a[1],
+                'question_type': a[2],
+                'question_id': a[3],
+                'question_slug': a[4],
+                'question_sort': a[5],
+                'answer': a[0],
             })
         if answers:
             answers = sorted(answers, key=lambda k: k['question_sort'])
